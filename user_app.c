bfa794e848a4c27302dca373f61a04676fae3449
yahia-abrini <yahia.abrini@ucalgary.ca>
2021-03-04 21:50:52 -0700

    5

--- user_app.c
+++ user_app.c
@@ -75,8 +75,14 @@ Promises:
 */
 void UserAppInitialize(void)
 {
-
-
+    /* LED Initialization */
+    LATA = 0x80;
+    
+    /* Timer0 control register initialization to turn timer on, asynch mode, 16 bit
+     * Fosc/4, 1:16 prescaler, 1:1 postscaler */
+    T0CON0 = 0x90; /* in binary: 1001 0000 */
+    T0CON1 = 0x54; /* in binary: 0101 0100 */
+    
 } /* end UserAppInitialize() */
 
   
@@ -96,10 +102,26 @@ void UserAppRun(void)
 {
     while(1)
     {
-    u32 u32Counter = FCY/2;
+    /* Read LATA to a temporary variable */
+    u32 u32Temporary = LATA; 
     
-    LATA=LATA+0x01;
-    __delay_ms(250);
+    /* initializing and incrementing static u16 variable */
+    static u16 u16Static = u16Static + 0x0001;
+    if(u16Static==500){
+        u16Static = 0x0000;
+    }
+    
+    u8 au8Pattern[0x01, 0x02, 0x04, 0x08, 0x10, 0x20];
+
+    
+    /* Use a bitmask and bitwise operation to clear the 6 LSBs */
+    u32Temporary = u32Temporary & 0x000000c0; 
+    
+    /* Use a bitwise operation to update the 6 LSBs to the new value you want */
+    u32Temporary = u32Temporary + 0x00000001;
+    
+    /* Write the temporary variable back to LATA */
+    LATA = u32Temporary;
     
     if(LATA==0xBF){
         LATA=0x80;
@@ -111,6 +133,32 @@ void UserAppRun(void)
 } /* end UserAppRun */
 
 
+
+/*--------------------------------------------------------------------
+void TimeXus(INPUT_PARAMETER_)
+Sets Timer0 to count u16Microseconds_
+Requires:
+- Timer0 configured such that each timer tick is 1 microsecond
+- INPUT_PARAMETER_ is the value in microseconds to time from 1 to 65535
+Promises:
+- Pre-loads TMR0H:L to clock out desired period
+- TMR0IF cleared
+- Timer0 enabled
+*/
+void TimeXus(INPUT_PARAMETER_)
+{
+    /* Disable the timer during config */
+    T0CON0 = T0CON0 ^ 0x80;
+    
+    /* Preload TMR0H and TMR0L based on u16TimeXus */
+    TMR0H = (2^16 - INPUT_PARAMETER_) & 0xf0;
+    TMR0L = (2^16 - INPUT_PARAMETER_) & 0x0f;
+    
+    /* Clear TMR0IF and enable Timer 0 */
+    PIR3 = PIR3 ^ 0x80;
+    T0CON0 = T0CON0 ^ 0x80;
+    
+} /* end TimeXus () */
 
 /*------------------------------------------------------------------------------------------------------------------*/
 /*! @privatesection */                                                                                            
